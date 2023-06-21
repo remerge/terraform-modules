49ee5816abfa0667d33cc1efcf0a29af9cf01019
Benedikt Böhm <bb@xnull.de>
2023-06-21 16:04:12 +0200

    Add missing outputs to google/sql/database

--- google/sql/database/outputs.tf
+++ google/sql/database/outputs.tf
@@ -1,5 +1,13 @@
+output "database" {
+  value = google_sql_database.main
+}
+
 output "user" {
   value = google_sql_user.main
+}
+
+output "password" {
+  value = random_password.user.result
 }
 
 output "secret" {
