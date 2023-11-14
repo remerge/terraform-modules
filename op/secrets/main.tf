534b87de81160217ac0d05d5415d5cc6997d9aaf
Benedikt Böhm <bb@xnull.de>
2023-11-14 13:24:33 +0000

    Add op/secrets module

---
+++ op/secrets/main.tf
@@ -0,0 +1,15 @@
+data "onepassword_vault" "secrets" {
+  name = "secrets"
+}
+
+resource "onepassword_item" "secrets" {
+  for_each = var.secrets
+  vault    = data.onepassword_vault.secrets.id
+  title    = "${var.prefix}_${each.key}"
+  category = "password"
+  password = each.value
+  tags = concat(var.tags, [
+    "prefix:${var.prefix}",
+    "workspace:${local.workspace.name}",
+  ])
+}
---
+++ op/secrets/variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  type = string
+}
+
+variable "tags" {
+  type    = list(string)
+  default = []
+}
+
+variable "secrets" {
+  type = map(string)
+}
