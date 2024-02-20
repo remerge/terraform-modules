8dceff27b6ca63b430abbb86b15a965b8114a39a
Benedikt Böhm <bb@xnull.de>
2024-02-20 10:41:19 +0100

    Update template and dependencies

--- google/kubernetes/main.tf
+++ google/kubernetes/main.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  version = "v29.0.0"
+  version = "v30.0.0"
 
   project_id = var.project
   name       = var.name
