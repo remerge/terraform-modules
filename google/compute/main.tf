1564c7d18f2be9a3d2a77188c4d40cf04fb86fab
Benedikt Böhm <bb@xnull.de>
2024-03-18 10:53:39 +0100

    Use id instead of self_link

--- google/compute/main.tf
+++ google/compute/main.tf
@@ -11,7 +11,7 @@ data "google_compute_instance_template" "main" {
 }
 
 resource "google_compute_instance_from_template" "main" {
-  source_instance_template = local.template.self_link
+  source_instance_template = local.template.id
 
   project = var.project
 
