9db0ff59d478dd6224ac550ce4088e7c512ebc20
Benedikt Böhm <bb@xnull.de>
2024-08-26 14:55:01 +0200

    Revert "Accept latest Leaseweb provider"
    This reverts commit ac0a8e3e394fb5457a323fd2b73ceb7d9096a5b3.

--- leaseweb/server/installation/versions.tf
+++ leaseweb/server/installation/versions.tf
@@ -3,7 +3,7 @@ terraform {
     # https://registry.terraform.io/providers/LeaseWeb/leaseweb/latest
     leaseweb = {
       source  = "LeaseWeb/leaseweb"
-      version = "~> 1.3"
+      version = "~> 0.4"
     }
   }
 }
