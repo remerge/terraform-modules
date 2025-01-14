d0ac65a7b80774325329278ef487ae39d610d157
Benedikt Böhm <bb@xnull.de>
2025-01-14 18:46:16 +0100

    Add missing brackets

--- google/sql/postgresql/main.tf
+++ google/sql/postgresql/main.tf
@@ -90,7 +90,7 @@ module "netbox-vm" {
 
   role     = "PostgreSQL"
   platform = "Google Cloud"
-  site     = coalesce(var.site, "Google Cloud ${google_sql_database_instance.main.region}"
+  site     = coalesce(var.site, "Google Cloud ${google_sql_database_instance.main.region}")
   cluster  = var.cluster
 
   interface  = "internal"
