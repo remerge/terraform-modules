2d76e2cd4d3de27386e45f459068a107f95b97b9
Olatunde Alex-Oni <olatunde.alexoni@remerge.io>
2025-07-08 14:48:30 +0200

    CORE-688 create nomad base module

---
+++ nomad/base/main.tf
@@ -0,0 +1,194 @@
+resource "nomad_namespace" "default" {
+  name        = "default"
+  description = "Default shared namespace"
+}
+
+resource "nomad_namespace" "diverts" {
+  name        = "diverts"
+  description = "Namespace for diverted deployments"
+}
+
+resource "nomad_node_pool" "default" {
+  name        = "default"
+  description = "Default node pool."
+}
+
+data "onepassword_item" "nomad_oidc_client" {
+  vault = data.onepassword_vault.secrets.uuid
+  title = "nomad_oidc_client"
+}
+
+resource "nomad_acl_auth_method" "okta" {
+  name           = "Okta"
+  type           = "OIDC"
+  token_locality = "global"
+  max_token_ttl  = var.auth_max_token_ttl
+  default        = true
+
+  token_name_format = "$${auth_method_type}-$${auth_method_name}"
+
+  config {
+    oidc_discovery_url = var.okta_discovery_url
+    oidc_client_id     = data.onepassword_item.nomad_oidc_client.username
+    oidc_client_secret = data.onepassword_item.nomad_oidc_client.password
+    oidc_scopes        = ["profile", "groups"]
+
+    allowed_redirect_uris = [
+      "http://localhost:4649/oidc/callback",
+      "http://nomad.service.${var.cluster}.consul:4646/ui/settings/tokens",
+      "http://nomad.app.${var.cluster}.rmge.net/ui/settings/tokens",
+    ]
+
+    claim_mappings = {
+      first_name = "first_name"
+      last_name  = "last_name"
+    }
+
+    list_claim_mappings = {
+      groups = "groups"
+    }
+  }
+}
+
+resource "nomad_acl_policy" "default" {
+  name        = "default"
+  description = "Default read-only policy"
+
+  rules_hcl = <<EOT
+node {
+  policy = "read"
+}
+
+namespace "${nomad_namespace.default.name}" {
+  policy = "read"
+  capabilities = ["alloc-exec", "alloc-lifecycle", "read-fs", "read-logs", "scale-job"]
+  variables {
+    path "*" {
+      capabilities = ["write", "read", "destroy", "list"]
+    }
+  }
+}
+
+namespace "${nomad_namespace.diverts.name}" {
+  policy = "write"
+  variables {
+    path "*" {
+      capabilities = ["write", "read", "destroy", "list"]
+    }
+  }
+}
+EOT
+}
+
+resource "nomad_acl_role" "default" {
+  name        = "default"
+  description = "Default role for users"
+
+  policy {
+    name = nomad_acl_policy.default.name
+  }
+}
+
+resource "nomad_acl_binding_rule" "default" {
+  auth_method = nomad_acl_auth_method.okta.name
+  bind_type   = "role"
+  bind_name   = nomad_acl_role.default.name
+}
+
+resource "nomad_acl_policy" "root" {
+  name        = "root"
+  description = "Default management policy for operators"
+
+  rules_hcl = <<EOT
+node {
+  policy = "write"
+}
+
+namespace "${nomad_namespace.default.name}" {
+  policy = "write"
+  variables {
+    path "*" {
+      capabilities = ["write", "read", "destroy", "list"]
+    }
+  }
+}
+
+namespace "${nomad_namespace.diverts.name}" {
+  policy = "write"
+  variables {
+    path "*" {
+      capabilities = ["write", "read", "destroy", "list"]
+    }
+  }
+}
+EOT
+}
+
+resource "nomad_acl_role" "root" {
+  name        = "root"
+  description = "Default role for operators"
+
+  policy {
+    name = nomad_acl_policy.root.name
+  }
+}
+
+resource "nomad_acl_binding_rule" "platform" {
+  auth_method = nomad_acl_auth_method.okta.name
+  bind_type   = "role"
+  bind_name   = nomad_acl_role.root.name
+  selector    = "\"Platform Team\" in list.groups"
+}
+
+resource "nomad_acl_policy" "github_actions" {
+  name        = "github-actions"
+  description = "Policy for Github Actions"
+
+  rules_hcl = <<EOT
+node {
+  policy = "write"
+}
+
+agent {
+  policy = "write"
+}
+
+operator {
+  policy = "write"
+}
+
+plugin {
+  policy = "list"
+}
+
+host_volume "*" {
+  policy = "write"
+}
+
+namespace "default" {
+  capabilities = ["submit-job","list-jobs","scale-job","dispatch-job"]
+  policy = "read"
+   variables {
+    path "*" {
+      capabilities = ["write", "read", "destroy", "list"]
+    }
+  }
+}
+EOT
+}
+
+resource "nomad_acl_token" "github_actions" {
+  name     = "Github Actions"
+  type     = "client"
+  policies = [nomad_acl_policy.github_actions.name]
+  global   = true
+}
+
+module "op_github_actions" {
+  source    = "github.com/remerge/terraform-modules//op/secrets?ref=v0.23.0"
+  workspace = var.workspace
+  prefix    = "nomad"
+  secrets = {
+    "token_${var.cluster}_github_actions" = nomad_acl_token.github_actions.secret_id
+  }
+}
