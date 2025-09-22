1d92e42e6c44dcd9e49003d45d7b42970aa5df37
Benedikt Böhm <bb@xnull.de>
2025-09-22 13:44:51 +0200

    Remove SendGrid 2fa_exempt scope

--- sendgrid/main.tf
+++ sendgrid/main.tf
@@ -2,7 +2,6 @@ resource "sendgrid_api_key" "main" {
   name = var.name
   scopes = [
     "mail.send",
-    "2fa_exempt",
     "2fa_required",
     "sender_verification_eligible",
     "sender_verification_legacy",
